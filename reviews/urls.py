b6d94d30087a08f791dbb006ee1714648df80c0b
DvirS123 <50909255+DvirS123@users.noreply.github.com>
2020-05-14 18:51:47 +0300

    login and signup working fine with database also now user can upload review

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('makereview/', views.makereview, name='makereview'),
+    path('all_reviews', views.all_reviews, name = 'all_reviews'),
+    path('<int:review_id>/', views.review_detailed, name='review_detailed'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Review
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
 	return render(request, 'home.html')
 
+@login_required
 def makereview(request):
-	return render(request, 'makereview.html')
+	if request.method == 'POST':
+		if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image']:
+			#only if all the fields are exist
+			review = Review()
+			review.Title = request.POST['title']
+			review.Body = request.POST['body']
+			try:
+				review.Source = request.POST['url']
+			except:
+				return render(request, 'makereview.html',{'error':"URL invalid"})
+			review.Image = request.FILES['image']
+			review.Date = timezone.datetime.now()
+			review.User_name = request.user
+			review.save()
+			return redirect('/Reviews/' + str(review.id))
+		else:
+			return render(request, 'makereview.html',{'error':"All field's must be filled!"})
+	else:
+		return render(request, 'makereview.html')
+
+
+def all_reviews(request):
+	reviews = Review.objects
+	return render(request,'all_reviews.html', {'reviews':reviews})
+
+def review_detailed(request, review_id):
+	detailreview = get_object_or_404(Review, pk=review_id)
+	return render(request,'review_detailed.html', {'review':detailreview})
